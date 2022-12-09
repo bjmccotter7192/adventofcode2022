5cad27b9f4d22edcfb8a2c5a692c646ca1042a3c
BJ McCotter <bjmccotter7192@gmail.com>
2022-12-09 09:42:26 -0600

    day 4 complete with one hiccup

--- day4.py
+++ day4.py
@@ -1,13 +1,27 @@
+# Advent of Code Day 4: Camp Cleanup
+# 
 
-
-with open("day4_test.txt", "r") as assignment_data:
-  # totalLines = assignment_data.readlines()
-  print("Let's get it for day 4")
+overlapped_assignments = 0
+with open("day4.txt", "r") as assignment_data:
   for line in assignment_data:
-    print(line)
-    seating_assignments = line.split(',')
-    elf1_assignment = range(seating_assignments[0].split('-')[0], seating_assignments[0].split('-')[1])
-    elf2_assignment = range(seating_assignments[1].split('-')[0], seating_assignments[0].split('-')[1])
+    stripped_line = line.strip()
+    seating_assignments = stripped_line.split(',')
 
-    print(elf1_assignment)
-    print(elf2_assignment)
+    elf1_assignment = [*range(int(seating_assignments[0].split('-')[0]), int(seating_assignments[0].split('-')[1]) + 1)]
+    elf2_assignment = [*range(int(seating_assignments[1].split('-')[0]), int(seating_assignments[1].split('-')[1]) + 1)]
+
+    if len(elf1_assignment) >= len(elf2_assignment):
+      # Question 1
+      result = all(assignment in elf1_assignment for assignment in elf2_assignment)
+      # Question 2
+      result = any(assignment in elf1_assignment for assignment in elf2_assignment)
+    else:
+      # Question 1
+      result = all(assignment in elf2_assignment for assignment in elf1_assignment)
+      # Question 2
+      result = any(assignment in elf2_assignment for assignment in elf1_assignment)
+
+    if result == True:
+      overlapped_assignments += 1
+
+print(overlapped_assignments)
